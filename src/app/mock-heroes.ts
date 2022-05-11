f894fda0d0c0b8815efa3117c201e47dff0c45a6
Jéssica Náyla Moreira Costa Barbosa <j.costa.barbosa@avanade.com>
2022-05-11 19:01:12 -0300

    primeiro commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Hulk'},
+  {id: 12, name: 'Batman'},
+  {id: 13, name: 'Wolverine'},
+  {id: 14, name: 'Mística'},
+  {id: 15, name: 'WandaVision'},
+  {id: 16, name: 'Peter Parker'},
+  {id: 17, name: 'Loki'},
+  {id: 18, name: 'Pantera Negra'},
+  {id: 19, name: 'Superman'},
+  {id: 20, name:'Deadpool'}
+];
